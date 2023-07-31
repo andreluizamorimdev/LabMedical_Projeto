30c343283471dde17f11c13da7ba4a492a1f0248
André Luiz Amorim de Souza <andreluizamorim.dev@gmail.com>
2023-07-31 17:56:39 -0300

    Adicionando verificação de auth em CadPacientePage

--- src/pages/CadastroPaciente/CadastroPaciente.page.tsx
+++ src/pages/CadastroPaciente/CadastroPaciente.page.tsx
@@ -1,13 +1,31 @@
+import { useEffect } from "react";
 import CadastroPacienteFormComponent from "../../components/Form/CadastroPaciente/CadastroPaciente.form.component";
+import { useAuth } from "../../hooks/useAuth";
+import { useToolbarContext } from "../../hooks/useToolbarContext";
 import * as Styled from './CadastroPaciente.style';
+import { Navigate } from "react-router-dom";
+
 const CadastroPacientePage = () => {
-    return (
-        <Styled.Container>
-            <Styled.Card>
-                <CadastroPacienteFormComponent />
-            </Styled.Card>    
-        </Styled.Container>
-    );
+    const { authentication } = useAuth();
+
+    const { setTitulo } = useToolbarContext();
+
+    useEffect(() => {
+        setTitulo('Cadastro de Paciente');
+    }, []);
+
+    const renderCadastroPacientePage = () => {
+        
+        return (
+            <Styled.Container>
+                <Styled.Card>
+                    <CadastroPacienteFormComponent />
+                </Styled.Card>    
+            </Styled.Container>
+        );
+    }
+
+    return authentication.isLogged ? renderCadastroPacientePage() : <Navigate to="/login" />;
 }
  
 export default CadastroPacientePage;
